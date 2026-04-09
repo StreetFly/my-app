ce8e67207fff372bb39262f8cd82edd80e2d24a9
StreetFly <ryannitroo@gmail.com>
2026-04-09 11:11:46 -0700

    Added counter and timer to demonstrate hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import Timer from './Timer';
 
 function App() {
   return (
@@ -15,9 +17,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Next Week
         </a>
       </header>
+      <Counter />
+      <Timer />
     </div>
   );
 }
